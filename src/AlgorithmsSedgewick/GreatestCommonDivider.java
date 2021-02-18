26bfe04ccbf76b3da2cfed024a55989bca5d3226
Evgeny Lyozin <evgenylyozindev@gmail.com>
2021-02-18 18:39:45 +0300

    Наибольший общий делитель

---
+++ src/AlgorithmsSedgewick/GreatestCommonDivider.java
@@ -0,0 +1,14 @@
+package AlgorithmsSedgewick;
+
+public class GreatestCommonDivider {
+
+    public static int gcd(int p, int q) {
+        if (q == 0) return p;
+        int r = p % q;
+        return gcd(q,r);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(gcd(900,250));
+    }
+}
