b64cc181cdf80562587fdd1094069a35c5e738b4
Evgeny Lyozin <evgenylyozindev@gmail.com>
2021-02-27 09:39:49 +0300

    Быстрая сортировка

--- src/AlgorithmsSedgewick/Quick3way.java
+++ src/AlgorithmsSedgewick/Quick3way.java
@@ -4,16 +4,24 @@ import edu.princeton.cs.algs4.In;
 import edu.princeton.cs.algs4.StdOut;
 import edu.princeton.cs.algs4.StdRandom;
 
-public class QuickSort {
+public class Quick3way {
     public static void sort(Comparable[] a) {
         StdRandom.shuffle(a);
         sort(a,0,a.length-1);
     }
     public static void sort(Comparable[] a, int lo, int hi) {
         if (hi<=lo) return;
-        int j = partition(a,lo,hi);
-        sort(a,lo,j-1);
-        sort(a,j+1,hi);
+        int lt = lo, i = lo+1, gt = hi;
+        Comparable v = a[lo];
+        while (i <= gt) {
+            int cmp = a[i].compareTo(v);
+            if (cmp < 0) exch(a,lt++, i++);
+            else if (cmp > 0) exch(a,i,gt--);
+            else i++;
+        }
+        sort(a,lo,lt-1);
+        sort(a,gt+1,hi);
+
     }
     private static int partition(Comparable[] a, int lo, int hi) {
         //Разбиение на a[lo..i-1], a[i], a[i+1..hi].
--- src/Exercises2_3/QuickSortWithInsertionSort.java
+++ src/Exercises2_3/QuickSortWithInsertionSort.java
@@ -1,16 +1,20 @@
-package AlgorithmsSedgewick;
+package Exercises2_3;
 
+import AlgorithmsSedgewick.InsertionSort;
 import edu.princeton.cs.algs4.In;
 import edu.princeton.cs.algs4.StdOut;
 import edu.princeton.cs.algs4.StdRandom;
 
-public class QuickSort {
+public class QuickSortWithInsertionSort {
     public static void sort(Comparable[] a) {
         StdRandom.shuffle(a);
         sort(a,0,a.length-1);
     }
     public static void sort(Comparable[] a, int lo, int hi) {
-        if (hi<=lo) return;
+        if (hi<=lo+7) {
+            InsertionSort.sort(a);
+            return;
+        };
         int j = partition(a,lo,hi);
         sort(a,lo,j-1);
         sort(a,j+1,hi);
